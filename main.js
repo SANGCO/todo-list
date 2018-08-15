4dac08693098a06946348f1db52b3f3aff62b957
sangco <nsg1213@gmail.com>
2018-08-15 22:30:53 +0900

    FEAT : todo 앞에 체크박스 만들기

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
+var totalItems = 0;
+
+function updateItemStatus() {
+    var chId = this.id.replace('cb_', "");
+    var itemText = document.getElementById('item_' + chId);
+
+    if (this.checked) {
+        itemText.className = 'checked';
+    } else {
+        itemText.className = "";
+    }
+}
+
 function addNewItem(list, itemText) {
+    totalItems++;
+
     var listItem = document.createElement('li');
-    listItem.innerText = itemText;
+    var checkBox = document.createElement('input');
+    checkBox.type = 'checkBox';
+    checkBox.id = 'cb_' + totalItems;
+    checkBox.onclick = updateItemStatus;
+
+    var span = document.createElement('span');
+    span.id = 'item_' + totalItems;
+    span.innerText = itemText;
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(span);
     list.appendChild(listItem);
 }
 
 var inputText = document.getElementById('inputText');
 inputText.focus();
 
-inputText.onkeyup = function(event) {
-    // Event.which (13) === ENTER key!!!
-    if(event.which === 13) {
+inputText.onkeyup = function(event){
+    //Event.which (13) === ENTER Key!!
+    if(event.which === 13){
         var itemText = inputText.value;
         if(!itemText || itemText === "" || itemText === " ") return false; // blank 방지
         addNewItem(document.getElementById('todolist'), itemText);
         inputText.focus();
         inputText.select();
     }
-}
+};
